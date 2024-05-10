a7f202a4ec17a4f310195496d12c15e1378e76ae
fesygdhuij <basharbotpsg@hotmail.com>
2024-05-11 01:13:45 +0700

    demo project

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-function isValidSudoku(board) {
-  const rows = new Array(9).fill().map(() => new Array(9).fill(0));
-  const cols = new Array(9).fill().map(() => new Array(9).fill(0));
-  const boxes = new Array(9).fill().map(() => new Array(9).fill(0));
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (board[i][j] !== ".") {
-        const num = Number(board[i][j]) - 1;
-        const k = Math.floor(i / 3) * 3 + Math.floor(j / 3);
-        if (rows[i][num] || cols[j][num] || boxes[k][num]) return false;
-        rows[i][num] = 1;
-        cols[j][num] = 1;
-        boxes[k][num] = 1;
-      }
+function levelOrderBottom(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
     }
+    result.unshift(currentLevel);
   }
-  return true;
+  return result;
 }
